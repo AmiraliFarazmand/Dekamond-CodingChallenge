e09654501b7b10ac5ef7d4f397a545c40f0a6225
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2025-02-14 01:47:52 +0330

    refactor [controller package]: code gets cleaner

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -9,5 +9,4 @@ func main() {
 	db.InitDB()
 	r := routes.SetupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080
-
 }
--- internal/app/controllers/table_controller.go
+++ internal/app/controllers/table_controller.go
@@ -4,24 +4,37 @@ import (
 	"net/http"
 	"resturant-task/internal/app/db"
 	"resturant-task/internal/app/models"
+	"resturant-task/internal/app/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
 const pricePerSeat float64 = 10.0
 
-func BookTable(c *gin.Context) {
-	type RequestBody struct {
-		Seats int `json:"seats" binding:"required,min=1"`
+type bookTableRequest struct {
+	Seats int `json:"seats" binding:"required,min=1"`
+}
+type cancelReservationRequest struct {
+	ReservationID uint `json:"reservation_id" binding:"required,min=1"`
+}
+
+func calculatePrice(seats, tableSeats int) float64 {
+	if seats == tableSeats {
+		return float64((tableSeats - 1)) * pricePerSeat
 	}
+	return float64(tableSeats) * pricePerSeat
+}
+
+func BookTable(c *gin.Context) {
+
 	// Get authenticated user from context
 	user, _ := c.Get("user") // in middleware we already check if user exists!
 	authUser := user.(models.User)
 
 	// Parse request body
-	var req RequestBody
+	var req bookTableRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		utils.RespondWithError(c, http.StatusBadRequest, "Invalid JSON body")
 		return
 	}
 
@@ -30,7 +43,7 @@ func BookTable(c *gin.Context) {
 	if err := db.DB.
 		Where("seats_number >= ? AND is_reserved = ?", req.Seats, false).
 		Order("seats_number ASC").First(&table).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "No available table found"})
+		utils.RespondWithError(c, http.StatusNotFound, "No available table found")
 		return
 	}
 
@@ -39,12 +52,7 @@ func BookTable(c *gin.Context) {
 	db.DB.Save(&table)
 
 	// Calculate price
-	var totalPrice float64
-	if req.Seats == table.SeatsNumber {
-		totalPrice = float64((table.SeatsNumber - 1)) * pricePerSeat
-	} else {
-		totalPrice = float64(table.SeatsNumber) * pricePerSeat
-	}
+	totalPrice := calculatePrice(req.Seats, table.SeatsNumber)
 
 	// Create reservation
 	reservation := models.Reservation{
@@ -67,48 +75,40 @@ func BookTable(c *gin.Context) {
 }
 
 func CancelReservation(c *gin.Context) {
-	// Request payload for canceling a reservation
-	type RequestBody struct {
-		ReservationID uint `json:"reservation_id" binding:"required,min=1"`
-	}
 
 	// Get authenticated user from context
 	user, _ := c.Get("user") // in middleware we already check if user exists!
 	authUser := user.(models.User)
 
 	// Parse request body
-	var req RequestBody
+	var req cancelReservationRequest
 	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+		utils.RespondWithError(c, http.StatusBadRequest, "Invalid request body")
 		return
 	}
 
-	// Find the reservation
 	var reservation models.Reservation
 	if err := db.DB.
 		Where("id = ? AND user_id = ?", req.ReservationID, authUser.ID).
 		First(&reservation).Error; err != nil {
-		c.JSON(http.StatusNotFound, gin.H{"error": "Reservation not found"})
+		utils.RespondWithError(c, http.StatusNotFound, "Reservation not found")
 		return
 	}
 
 	// Check if the reservation is already inactive
 	if !reservation.IsActive {
-		c.JSON(http.StatusBadRequest, gin.H{"error": "Reservation already canceled"})
+		utils.RespondWithError(c, http.StatusBadRequest, "Reservation already canceled")
 		return
 	}
 
-	// Mark reservation as inactive
+	// Mark reservation as inactive and free up the table
 	reservation.IsActive = false
 	db.DB.Save(&reservation)
-
-	// Free up the table
 	var table models.Table
 	if err := db.DB.First(&table, reservation.TableID).Error; err == nil {
 		table.IsReserved = false
 		db.DB.Save(&table)
 	}
 
-	// Return success response
 	c.JSON(http.StatusOK, gin.H{"message": "Reservation canceled successfully"})
 }
