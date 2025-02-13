ba527c83f4503c0db086eee6bce9aff05f8eabfa
Amirali Farazmand <75451772+AmiraliFarazmand@users.noreply.github.com>
2025-02-13 20:46:20 +0330

    refactor [routes package]: create routes package. clean main file

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,40 +1,14 @@
 package main
 
 import (
-	"log"
-	"lotus-task/internal/app/controllers"
-	"lotus-task/internal/app/db"
-	"lotus-task/internal/app/middleware"
 
-	"github.com/gin-gonic/gin"
+	"lotus-task/internal/app/db"
+	"lotus-task/internal/app/routes"
 )
 
-func init() {
-	err := db.Connect()
-	if err != nil {
-		log.Println(err)
-	}
-	db.RunMigrations(db.DB)
-	log.Println("successful to run migrations!")
-}
-
 func main() {
-	r := gin.Default()
-	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})
-	r.POST("/signup", controllers.Signup)
-	r.POST("/login", controllers.Login)
-	r.GET("/validate", middleware.RequireAuth, controllers.ValidateIsAuthenticated)
-	r.POST("/book", middleware.RequireAuth, controllers.BookTable)
-	r.POST("/cancel", middleware.RequireAuth, controllers.CancelReservation)
+	db.InitDB()
+	r := routes.SetupRouter()
 	r.Run() // listen and serve on 0.0.0.0:8080
 
 }
---
+++ internal/app/routes/routes.go
@@ -0,0 +1,35 @@
+package routes
+
+import (
+	"lotus-task/internal/app/controllers"
+	"lotus-task/internal/app/middleware"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter() *gin.Engine {
+	r := gin.Default()
+
+	// CORS Middleware
+	r.Use(func(c *gin.Context) {
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+		c.Next()
+	})
+
+	// Auth Routes
+	r.POST("/signup", controllers.Signup)
+	r.POST("/login", controllers.Login)
+	r.GET("/validate", middleware.RequireAuth, controllers.ValidateIsAuthenticated)
+
+	// Reservation Routes
+	r.POST("/book", middleware.RequireAuth, controllers.BookTable)
+	r.POST("/cancel", middleware.RequireAuth, controllers.CancelReservation)
+
+	return r
+}
